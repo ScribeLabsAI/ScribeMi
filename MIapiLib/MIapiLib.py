d82406dfcaa49efbdb6186458867574646a22eab
ailinvenerus <ailinvenerus@gmail.com>
2022-08-01 15:54:21 +0100

    Modifying generate_upload_link, adding submit_MI and delete_MI. Method get_MI pending. Updating camelCase to snake_case

--- MIapiLib/MIapiLib.py
+++ MIapiLib/MIapiLib.py
@@ -1,49 +1,67 @@
-import os
 import requests
-from dotenv import load_dotenv
-
-load_dotenv()
+import json
+from io import BytesIO
+from typing import BinaryIO, Optional
+from typing import Union
 
 class MI:
-    def __init__(self, environment: str, id_token: str):
-        self.headers = {'Authorization' : id_token}
-        if environment.upper() == 'DEV':
-            self.url = os.environ.get("DEV_URL")
-            self.headers.update({'x-api-key' : os.environ.get("DEV_API_KEY")})
-        elif environment.upper() == 'PROD':
-            self.url = os.environ.get("PROD_URL")
-            self.headers.update({'x-api-key' : os.environ.get("PROD_API_KEY")})
-        else:
-            raise Exception("Invalid environment. Use \'DEV\' or \'PROD\' instead")
+    def __init__(self, id_token: str, url: str, apiKey: str):
+        self.url = url
+        self.headers = {'Authorization' : id_token, 'x-api-key' : apiKey}
 
-    def listArchives(self) -> requests.Response:
+    def update_idtoken(self, id_token: str) -> None:
+        self.headers.update({'Authorization' : id_token})
+
+    def list_archives(self) -> requests.Response:
         return requests.get(self.url + '/archives', headers=self.headers)
 
-    def deleteArchive(self, filename: str) -> requests.Response:
+    def delete_archive(self, filename: str) -> requests.Response:
         return requests.delete(self.url + '/archive', headers=self.headers, json={'key': filename})
 
+    def generate_upload_link(self, file_or_filename: Union[str, BytesIO, BinaryIO]) -> requests.Response:
+        url = self.__get_url_generate_upload_link()
+        if isinstance(file_or_filename, str):
+            file = open(file_or_filename, 'rb')
+        else:
+            file = file_or_filename
+        return requests.put(url, headers={'Content-Type' : 'application/zip'}, files={'file': file})
 
-# TODO: MAKE IT ACCEPT A CONTENT FILE OR A FILENAME
-    def generateUploadLink(self, file) -> requests.Response:
-        url = requests.get(self.url + '/archive', headers=self.headers).text[1:][:-1]
-        # TODO: ADD FILE OR FILENAME
-        return requests.put(url, headers={'Content-Type' : 'application/zip'}, files={'file': open(file, 'rb')})
+    def submit_MI(self, file_or_filename: Union[str, BytesIO, BinaryIO], filetype, filename: Optional[str] = None) -> requests.Response:
+        filetype_list = ['pdf', 'xlsx', 'xls', 'xlsm', 'doc', 'docx', 'ppt', 'pptx']
+        if filetype not in filetype_list:
+            raise Exception("Invalid filetype. Accepted values: 'pdf', 'xlsx', 'xls', 'xlsm', 'doc', 'docx', 'ppt', 'pptx'.")
+        if isinstance(file_or_filename, str):
+            file = open(file_or_filename, 'rb')
+        else:
+            file = file_or_filename
+        headers = self.headers.copy()
+        headers.update({'Content-Type' : 'application/json'})
+        if filename is None:
+            body = {'filetype': filetype}
+        else:
+            body = {'filename': filename, 'filetype': filetype}
+        url = self.__get_url_submit_MI(headers, body)
+        return requests.put(url, headers={'Content-Type' : 'application/pdf'}, files={'file': file})
+       
+    def delete_MI(self, jobid: str) -> requests.Response:
+        body = {'jobid': jobid}
+        return requests.delete(self.url + '/mi', headers=self.headers, json=body)
 
+# TODO: CONTINUE WITH THIS: API yet not ready
+    # def get_MI(self, jobid: str) -> requests.Response:
+    #     request = requests.get(self.url + '/mi/' + jobid, headers=self.headers)
+    #     return request
 
-id_token = ''
-mi = MI('dev', id_token)
-# -------------------------------------------------------------------
-archives = mi.listArchives()
-print(archives.text)
-print(archives.status_code)
-# -------------------------------------------------------------------
-# deleteRequest = mi.deleteArchive('name.zip')
-# print(deleteRequest.headers)
-# print(deleteRequest.text)
-# print(deleteRequest.status_code)
-# -------------------------------------------------------------------
-# file = 'path'
-# linkToUpload = mi.generateUploadLink(file)
-# print(linkToUpload.headers)
-# print(linkToUpload.text)
-# print(linkToUpload.status_code)
+    def __get_url_generate_upload_link(self) -> str:
+        request = requests.get(self.url + '/archive', headers=self.headers)
+        if request.status_code == 401:
+            raise Exception('The current token has expired. Update it.')
+        else:
+            return json.loads(request.content)
+
+    def __get_url_submit_MI(self, headers, body) -> str:
+        request = requests.post(self.url + '/mi', headers=headers, json=body)
+        if request.status_code == 401:
+            raise Exception('The current token has expired. Update it.')
+        else:
+            return json.loads(request.content).get('url')
