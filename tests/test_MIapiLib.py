f3e111ca52b97aeb7025c8ac36732793a6bc2df7
ailinvenerus <ailinvenerus@gmail.com>
2022-08-02 17:58:34 +0100

    Adding some tests for create_job. More tests pending to be continued tomorrow

--- tests/test_MIapiLib.py
+++ tests/test_MIapiLib.py
@@ -14,6 +14,8 @@ id_token: str = access.get_tokens(username=os.environ.get("USER"), password=os.e
 url: str = os.environ.get("URL")
 api_key: str = os.environ.get("API_KEY")
 archive_path = 'tests/example.zip'
+file_name = 'example.pdf'
+file_path = 'tests/' + file_name
 
 mi = MI(url, api_key)
 mi.update_id_token(id_token)
@@ -23,7 +25,7 @@ class TestMIapiLibArchives(unittest.TestCase):
     def test_upload_archive_filename_1(self):
         clear_archives(mi)
         mi.upload_archive(archive_path)
-        assert_file_amount_uploaded(self, mi, 1)
+        self.assertEquals(1, len(mi.list_archives()))
         clear_archives(mi)
 
     def test_upload_archive_filename_2(self):
@@ -32,27 +34,58 @@ class TestMIapiLibArchives(unittest.TestCase):
         time.sleep(1)
         mi.upload_archive(archive_path)
         time.sleep(1)
-        assert_file_amount_uploaded(self, mi, 2)
+        self.assertEquals(2, len(mi.list_archives()))
         clear_archives(mi)
 
-    def test_upload_archive_file_content_1(self):
+    def test_upload_archive_filecontent_1(self):
         clear_archives(mi)
         mi.upload_archive(open(archive_path, 'rb'))
-        assert_file_amount_uploaded(self, mi, 1)
+        self.assertEquals(1, len(mi.list_archives()))
         clear_archives(mi)
 
-    def test_upload_archive_file_content_2(self):
+    def test_upload_archive_filecontent_2(self):
         clear_archives(mi)
-        mi.upload_archive(open(archive_path, 'rb'))
-        time.sleep(1)
         mi.upload_archive(open(archive_path, 'rb'))
         time.sleep(1)
-        assert_file_amount_uploaded(self, mi, 2)
+        mi.upload_archive(open(archive_path, 'rb'))
+        time.sleep(1)
+        self.assertEquals(2, len(mi.list_archives()))
         clear_archives(mi)
 
 
-def assert_file_amount_uploaded(self, mi, amount):
-    self.assertEquals(amount, len(mi.list_archives()))
+class TestMIapiLibJob(unittest.TestCase):
+    def test_create_job_filepath_format_and_name(self):
+        job_created = mi.create_job(file_path, 'pdf', file_name)
+        time.sleep(1)
+        job = mi.get_job(job_created.get('jobid'))
+        self.assertEquals(file_name, job.get('filename'))
+        self.assertEquals('PENDING', job.get('status'))
+
+    def test_create_job_filepath_format_and_no_name(self):
+        job_created = mi.create_job(file_path, 'pdf')
+        time.sleep(1)
+        job = mi.get_job(job_created.get('jobid'))
+        self.assertEquals('', job.get('filename'))
+        self.assertEquals('PENDING', job.get('status'))
+
+    def test_create_job_filecontent_format_and_name(self):
+        job_created = mi.create_job(open(file_path, 'rb'), 'pdf', file_name)
+        time.sleep(1)
+        job = mi.get_job(job_created.get('jobid'))
+        self.assertEquals(file_name, job.get('filename'))
+        self.assertEquals('PENDING', job.get('status'))
+
+    def test_create_job_filecontent_format_and_no_name(self):
+        job_created = mi.create_job(open(file_path, 'rb'), 'pdf')
+        time.sleep(1)
+        job = mi.get_job(job_created.get('jobid'))
+        self.assertEquals('', job.get('filename'))
+        self.assertEquals('PENDING', job.get('status'))
+
+    def test_create_job_filecontent_wrong_format(self):
+        with self.assertRaises(Exception):
+            self.assertRaises(mi.create_job(open(file_path, 'rb'), 'pdff'))
+        
         
 def clear_archives(mi):
     archives = mi.list_archives()
