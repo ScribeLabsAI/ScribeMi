a2f96d44c647d69264adb255e1a64724b594e691
ailinvenerus <ailinvenerus@gmail.com>
2022-08-02 14:37:50 +0100

    Making requested changes

--- MIapiLib/MIapiLib.py
+++ MIapiLib/MIapiLib.py
@@ -1,36 +1,54 @@
 import requests
 import json
 from io import BytesIO
-from typing import BinaryIO, Optional, TypedDict, Union
+from typing import BinaryIO, List, Optional, TypedDict, Union
 
-class MIItem(TypedDict):
+class Job(TypedDict):
     filename: str
     status: str
     url: Optional[str]
 
-class MI:
-    def __init__(self, id_token: str, url: str, apiKey: str):
-        self.url = url
-        self.headers = {'Authorization' : id_token, 'x-api-key' : apiKey}
+class Archive(TypedDict):
+    name: str
+    url: str
+    last_modified: str
 
-    def update_idtoken(self, id_token: str) -> None:
+class MI:
+    def __init__(self, url: str, api_key: str):
+        self.url = url
+        self.headers = {'x-api-key' : api_key}
+
+    def update_id_token(self, id_token: str) -> None:
         self.headers.update({'Authorization' : id_token})
 
-    def list_archives(self) -> requests.Response:
-        return requests.get(self.url + '/archives', headers=self.headers)
+    def list_archives(self) -> list[Archive]:
+        response = requests.get(self.url + '/archives', headers=self.headers)
+        self.__validate_response(response)
+        body = json.loads(response.text).get('archives')
+        archives: List = []
+        for a in body:
+            archives.append(Archive(name=a.get('key'), url=a.get('link'), last_modified=a.get('lastModified')))
+        return archives
 
-    def delete_archive(self, filename: str) -> requests.Response:
-        return requests.delete(self.url + '/archive', headers=self.headers, json={'key': filename})
+    def delete_archive(self, filename: str) -> True:
+        response = requests.delete(self.url + '/archive', headers=self.headers, json={'key': filename})
+        self.__validate_response(response)
+        return True
 
-    def generate_upload_link(self, file_or_filename: Union[str, BytesIO, BinaryIO]) -> requests.Response:
-        url = self.__get_url(requests.get(self.url + '/archive', headers=self.headers))
+    def upload_archive(self, file_or_filename: Union[str, BytesIO, BinaryIO]) -> True:
+        response_get_link = requests.get(self.url + '/archive', headers=self.headers)
+        self.__validate_response(response_get_link)
+        url = json.loads(response_get_link.content)
+        print('URL: {}'.format(url))
         if isinstance(file_or_filename, str):
             file = open(file_or_filename, 'rb')
         else:
             file = file_or_filename
-        return requests.put(url, headers={'Content-Type' : 'application/zip'}, files={'file': file})
+        response = requests.put(url, headers={'Content-Type' : 'application/zip'}, files={'file': file})
+        self.__validate_response(response)
+        return True
 
-    def submit_MI(self, file_or_filename: Union[str, BytesIO, BinaryIO], filetype, filename: Optional[str] = None) -> requests.Response:
+    def create_job(self, file_or_filename: Union[str, BytesIO, BinaryIO], filetype, filename: Optional[str] = None) -> True:
         filetype_list = ['pdf', 'xlsx', 'xls', 'xlsm', 'doc', 'docx', 'ppt', 'pptx']
         if filetype not in filetype_list:
             raise Exception("Invalid filetype. Accepted values: 'pdf', 'xlsx', 'xls', 'xlsm', 'doc', 'docx', 'ppt', 'pptx'.")
@@ -40,33 +58,36 @@ class MI:
             file = file_or_filename
         headers = self.headers.copy()
         headers.update({'Content-Type' : 'application/json'})
-        if filename is None:
-            body = {'filetype': filetype}
-        else:
-            body = {'filename': filename, 'filetype': filetype}
-        url = self.__get_url(requests.post(self.url + '/mi', headers=headers, json=body)).get('url')
-        return requests.put(url, headers={'Content-Type' : 'application/pdf'}, files={'file': file})
+        body = {'filetype': filetype}
+        if filename is not None:
+            body.update({'filename': filename})
+        response_get_link = requests.post(self.url + '/mi', headers=headers, json=body)
+        self.__validate_response(response_get_link)
+        url = json.loads(response_get_link.content).get('url')
+        response = requests.put(url, headers={'Content-Type' : 'application/pdf'}, files={'file': file})
+        self.__validate_response(response)
+        return True
        
-    def delete_MI(self, jobid: str) -> requests.Response:
+    def delete_job(self, jobid: str) -> True:
         body = {'jobid': jobid}
-        return requests.delete(self.url + '/mi', headers=self.headers, json=body)
+        response = requests.delete(self.url + '/mi', headers=self.headers, json=body)
+        self.__validate_response(response)
+        return True
 
-    def get_MI(self, jobid: str) -> MIItem:
+    def get_job(self, jobid: str) -> Job:
         response = requests.get(self.url + '/mi/', headers=self.headers, params={'jobid' : jobid})
-        if response.status_code == 404 or response.status_code == 500: # TODO: UPDATE, CURRENTLY IT'S 500 FOR THIS CASE BUT IT SHOULD BE 404
-            raise Exception('The jobid {} does not exist'.format(jobid))
-        else:
-            body = self.__get_url(response)
+        self.__validate_response(response)
+        body = json.loads(response.content)
+        job = Job(filename=body.get('filename'), status=body.get('status'))
         if body.get('status') == 'SUCCESS':
-            mi = MIItem(filename=body.get('filename'), status=body.get('status'), url=body.get('url'))
-        else:
-            mi = MIItem(filename=body.get('filename'), status=body.get('status'))
-        return mi
+            job.update({'url' : body.get('url')})
+        return job
 
-    def __get_url(self, response: requests.Response) -> str:
-        if response.status_code == 401:
-            raise Exception('The current token has expired. Update it.')
-        elif response.status_code == 200:
-            return json.loads(response.content)
-        else:
-            raise Exception('An error ocurred, try again later.')
+    def __validate_response(self, response: requests.Response):
+        if response.status_code != 200:
+            if response.status_code == 401:
+                raise Exception('The current token has expired. Update it.')
+            if response.status_code == 404:
+                raise Exception('Bad request.')
+            else:
+                raise Exception('An error ocurred, try again later.')
