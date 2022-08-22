0f4d8e207477d608de3229a940b975b4b12c738a
ailinvenerus <ailinvenerus@gmail.com>
2022-08-22 14:29:39 +0100

    Fixing conf.py

--- docs_source/conf.py
+++ docs_source/conf.py
@@ -12,7 +12,7 @@ import sys
 
 project = 'Scribe Mi'
 copyright = '2022, Scribe Labs Limited'
-author = 'Scribe Labs Limited'
+author = 'Ailin Venerus'
 release = '1.1.1'
 
 # -- General configuration ---------------------------------------------------
