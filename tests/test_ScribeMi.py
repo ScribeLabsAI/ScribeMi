884b166bb0ea88865d6ad3e15c28ad2ab276564e
Alex Bostock <alex@scribelabs.ai>
2023-09-08 11:04:23 +0100

    Rebuild SDK to support API v1

--- tests/test_ScribeMi.py
+++ tests/test_ScribeMi.py
@@ -15,147 +15,84 @@ Mandatory env vars to set:
 URL, API_KEY, CLIENT_ID, USER, PASSWORD, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_SESSION_TOKEN, REGION_NAME
 """
 
-client_id: str = os.environ.get("CLIENT_ID")
-username: str = os.environ.get("USER")
-password: str = os.environ.get("PASSWORD")
-access = ScribeAuth(client_id)
-id_token: str = access.get_tokens(username=os.environ.get("USER"), password=os.environ.get("PASSWORD")).get('id_token')
-url: str = os.environ.get("URL")
-api_key: str = os.environ.get("API_KEY")
-archive_path = 'tests/companies_house_document.zip'
-file_name = 'companies_house_document.pdf'
-file_path = 'tests/' + file_name
-company_name='Company Name'
-mi = MI(api_key, url)
-mi.update_id_token(id_token)
-jobid_list = []
+env = {
+    'API_URL': os.environ['API_URL'],
+    'IDENTITY_POOL_ID': os.environ['IDENTITY_POOL_ID'],
+    'USER_POOL_ID': os.environ['USER_POOL_ID'],
+    'CLIENT_ID': os.environ['CLIENT_ID'],
+    'REGION': os.environ['REGION'],
+}
 
-class TestScribeMiArchives(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(self) -> None:
-        clear_archives(mi)
-
-    def test_upload_archive_filename_1(self):
-        archive_name = mi.upload_archive(archive_path)
-        archives_list_names = [a.get('name') for a in mi.list_archives()]
-        self.assertTrue(archive_name in archives_list_names)
-
-    def test_upload_archive_filename_2(self):
-        archive_name = mi.upload_archive(archive_path)
-        time.sleep(1)
-        with open(archive_path, 'rb') as f:
-            archive_name2 = mi.upload_archive(f)
-        time.sleep(1)
-        archives_list_names = [a.get('name') for a in mi.list_archives()]
-        self.assertTrue(archive_name in archives_list_names)
-        self.assertTrue(archive_name2 in archives_list_names)
-
-    def test_upload_archive_filecontent_1(self):
-        with open(archive_path, 'rb') as f:
-            archive_name = mi.upload_archive(f)
-        archives_list_names = [a.get('name') for a in mi.list_archives()]
-        self.assertTrue(archive_name in archives_list_names)
-
-    def test_upload_archive_filecontent_2(self):
-        with open(archive_path, 'rb') as f:
-                archive_name = mi.upload_archive(f)
-        time.sleep(1)
-        with open(archive_path, 'rb') as f:
-                archive_name2 = mi.upload_archive(f)
-        time.sleep(1)
-        archives_list_names = [a.get('name') for a in mi.list_archives()]
-        self.assertTrue(archive_name in archives_list_names)
-        self.assertTrue(archive_name2 in archives_list_names)
-
-    def test_upload_archive_wrong_token_raise_exception(self):
-        mi_wrong_token = MI(api_key, url)
-        mi_wrong_token.update_id_token('wrong_id_token')
-        self.assertRaises(UnauthenticatedException, mi_wrong_token.upload_archive, archive_path)
-            
-    @classmethod
-    def tearDownClass(self) -> None:
-        clear_archives(mi)
+username_and_password = {
+    'username': os.environ['USERNAME'],
+    'password': os.environ['PASSWORD'],
+}
 
 
-class TestScribeMiJob(unittest.TestCase):
+class TestScribeMiAuth(unittest.TestCase):
+    def test_fetch_credentials_with_username_and_password(self):
+        client = MI(env)
+        client.authenticate(username_and_password)
 
-    @classmethod
-    def setUpClass(self) -> None:
-        clear_jobs(mi)
+    def test_fetch_credentials_with_refresh_token(self):
+        client = MI(env)
+        client.authenticate(username_and_password)
+        client.authenticate({ 'refresh_token': client.tokens['refresh_token'] })
 
-    def test_create_job_filepath_format_and_name(self):
-        job_created = mi.create_job(company_name, file_path, 'pdf', file_name)
-        jobid_list.append(job_created.get('jobid'))
-        time.sleep(1)
-        job = mi.get_job(job_created.get('jobid'))
-        self.assertEqual(file_name, job.get('filename'))
-        self.assertEqual('PENDING', job.get('status'))
-        self.assertEqual(None, job.get('url'))
+    def test_reauthenticate(self):
+        client = MI(env)
+        client.authenticate(username_and_password)
+        client.reauthenticate()
 
-    def test_create_job_filepath_format_and_no_name(self):
-        job_created = mi.create_job(company_name, file_path, 'pdf')
-        jobid_list.append(job_created.get('jobid'))
-        time.sleep(1)
-        job = mi.get_job(job_created.get('jobid'))
-        self.assertEqual('', job.get('filename'))
-        self.assertEqual('PENDING', job.get('status'))
-        self.assertEqual(None, job.get('url'))
+    def test_authenticated_api_call(self):
+        client = MI(env)
+        client.authenticate(username_and_password)
+        client.list_tasks()
 
-    def test_create_job_filecontent_format_and_name(self):
-        with open(file_path, 'rb') as f:
-            job_created = mi.create_job(company_name, f, 'pdf', file_name)
-        jobid_list.append(job_created.get('jobid'))
-        time.sleep(1)
-        job = mi.get_job(job_created.get('jobid'))
-        self.assertEqual(file_name, job.get('filename'))
-        self.assertEqual('PENDING', job.get('status'))
-        self.assertEqual(None, job.get('url'))
+    def test_throws_if_not_authenticated(self):
+        client = MI(env)
+        try:
+            client.list_tasks()
+            assert False
+        except UnauthenticatedException:
+            assert True
 
-    def test_create_job_filecontent_format_and_no_name(self):
-        with open(file_path, 'rb') as f:
-            job_created = mi.create_job(company_name, f, 'pdf')
-        jobid_list.append(job_created.get('jobid'))
-        time.sleep(1)
-        job = mi.get_job(job_created.get('jobid'))
-        self.assertEqual('', job.get('filename'))
-        self.assertEqual('PENDING', job.get('status'))
-        self.assertEqual(None, job.get('url'))
+class TestScribeMiErrorHandling(unittest.TestCase):
+    def test_throws_on_error_response(self):
+        client = MI(env)
+        client.authenticate(username_and_password)
+        try:
+            client.get_task('invalidJobid')
+            assert False
+        except:
+            assert True
 
-    def test_create_job_filecontent_wrong_format_raise_exception(self):
-        with open(file_path, 'rb') as f:
-            self.assertRaises(InvalidFiletypeException, mi.create_job, company_name, f, 'pdff')
+class TestScribeMiEndpoints(unittest.TestCase):
+    def test_endpoints(self):
+        client = MI(env)
+        client.authenticate(username_and_password)
 
-    def test_get_job_non_existent_raise_exception(self):
-        self.assertRaises(TaskNotFoundException, mi.get_job, 'jobid_test')
+        jobid = client.submit_task('tests/companies_house_document.pdf', {
+            'filetype': 'pdf',
+        })
 
-    @classmethod   
-    def tearDownClass(self) -> None:
-        clear_jobs(mi)
-        clear_db_table()
+        client.list_tasks()
 
+        task = client.get_task(jobid)
 
-def clear_archives(mi: MI):
-    archives = mi.list_archives()
-    for a in archives:
-        mi.delete_archive(a.get('name'))
+        try:
+            client.fetch_model(task)
+            assert False
+        except Exception:
+            # TODO: test error message
+            # Error because task has not been processed
+            assert True
 
-def clear_jobs(mi: MI):
-    for j in jobid_list:
-        mi.delete_job(j)
+        try:
+            client.consolidate_tasks([task])
+            assert False
+        except Exception:
+            # Error because the model is not ready
+            assert True
 
-def clear_db_table():
-    jwt = JWT()
-    userid = jwt.decode(message=id_token, algorithms=['RS256'], do_verify=False).get('sub')
-    clientDynamoDB = boto3.client('dynamodb', region_name='eu-west-2')
-    all_jobid = clientDynamoDB.scan(TableName='tasks', ScanFilter={
-        'userid':{
-            'AttributeValueList':[{'S':str(userid)}],
-            'ComparisonOperator':'EQ'
-        }
-    }).get('Items')
-    for j in all_jobid:
-        clientDynamoDB.delete_item(
-            TableName='tasks',
-            Key={'jobid':{'S':str(j.get('jobid').get('S'))}}
-    )
+        client.delete_task(task)
